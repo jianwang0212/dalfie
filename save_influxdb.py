cd787f74138311b7c10dec841905655dbeadc2e3
zi_remote <jianwang0212@gmail.com>
2021-12-29 12:34:23 +0000

    relative path

--- save_influxdb.py
+++ save_influxdb.py
@@ -6,8 +6,8 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
-
-df = pd.read_csv("/Users/Zi/Projects/Dalfie/one_week_gas——bsc.csv")
+import os
+df = pd.read_csv(os.getcwd() + "/one_week_gas——bsc.csv")
 df2 = df[['timeStamp', 'hash','exchange','blockNumber','gas_fee']]
 df2.set_index('timeStamp',inplace=True)
 df2.index = pd.to_datetime(df2.index, unit = "s", utc=True)
